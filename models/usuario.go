937b1d8113b1d14f37952b8c45718819abbc9627
Daniel Fonseca <daniel.fonseca@dgtallab.com.br>
2024-04-18 11:51:04 +0100

    feat: add methods validation user

--- models/usuario.go
+++ models/usuario.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Usuario struct {
 	ID       uint64    `json:"id, omitempty"`
@@ -9,4 +13,36 @@ type Usuario struct {
 	Email    string    `json:"email, omitempty"`
 	Senha    string    `json:"senha, omitempty"`
 	CriadoEm time.Time `json:"CriadoEm, omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	usuario.formatar()
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("nome é obrigatório e não pode estar em branco")
+	}
+	if usuario.Nick == "" {
+		return errors.New("nick é obrigatório e não pode estar em branco")
+	}
+	if usuario.Email == "" {
+		return errors.New("email é obrigatório e não pode estar em branco")
+	}
+	if usuario.Senha == "" {
+		return errors.New("senha é obrigatória e não pode estar em branco")
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }
